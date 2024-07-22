c03f20cec3e9c6a3e117dbfd9df72890436d6539
Rajat Chauhan <chauhanrajat.work@gmail.com>
2024-07-22 17:53:45 +0000

    addimg files with initial commit

---
+++ java/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("Hello, Docker! Your Machine current date: " + currentDate);
+    }
+}
